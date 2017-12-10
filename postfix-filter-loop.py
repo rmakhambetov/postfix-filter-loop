21895612f6b914dda1c078f7ed41adbb981bd447
rmakhambetov <rmakhambetov@yandex.ru>
2017-12-11 02:15:51 +0600

    Update postfix-filter-loop.py

--- postfix-filter-loop.py
+++ postfix-filter-loop.py
@@ -6,8 +6,10 @@ import smtplib
 import traceback
 
 MAX_DAYS = 15
-HOST = 'localhost'
-PORT = 10025
+FILTER_HOST = '127.0.0.1'
+FILTER_PORT = 10025
+POSTFIX_HOST = '127.0.0.1'
+POSTFIX_PORT = 10026
 
 class CustomSMTPServer(smtpd.SMTPServer):
 
@@ -20,19 +22,19 @@ class CustomSMTPServer(smtpd.SMTPServer):
 			recipient.replace('\'', '')
 			recipient.replace('\"', '')
 		
-#		print 'Receiving message from:', peer
-#		print 'Message addressed from:', mailfrom
-#		print 'Message addressed to  :', rcpttos
-#		print 'MSG >>'
-#		print data
-#		print '>> EOT'
+#		print('Receiving message from:', peer)
+#		print('Message addressed from:', mailfrom)
+#		print('Message addressed to  :', rcpttos)
+#		print('MSG >>')
+#		print(data)
+#		print('>> EOT')
 
 		try:
 			extracted = tldextract.extract(mailfrom)
                         domain_info = pythonwhois.get_whois("%s.%s" % (extracted.domain, extracted.suffix))
                         delta = datetime.datetime.now() - domain_info.get('creation_date', [datetime.datetime.now()])[0]
                         if delta.days < MAX_DAYS:
-                            print 'Prevent delivery from suspicious sender: %s' % (maifrom)
+                            print('Prevent delivery from suspicious sender: %s' % (maifrom))
                             return
 		except:
 			pass
@@ -40,43 +42,43 @@ class CustomSMTPServer(smtpd.SMTPServer):
 			print traceback.format_exc()
 
 		try:
-			server = smtplib.SMTP('localhost', 10026)
+			server = smtplib.SMTP(POSTFIX_HOST, POSTFIX_PORT)
 			server.sendmail(mailfrom, rcpttos, data)
 			server.quit()
-#			print 'send successful'
+#			print('send successful')
 		except smtplib.SMTPException:
-			print 'Exception SMTPException'
+			print('Exception SMTPException')
 			pass
 		except smtplib.SMTPServerDisconnected:
-			print 'Exception SMTPServerDisconnected'
+			print('Exception SMTPServerDisconnected')
 			pass
 		except smtplib.SMTPResponseException:
-			print 'Exception SMTPResponseException'
+			print('Exception SMTPResponseException')
 			pass		
 		except smtplib.SMTPSenderRefused:
-			print 'Exception SMTPSenderRefused'
+			print('Exception SMTPSenderRefused')
 			pass		
 		except smtplib.SMTPRecipientsRefused:
-			print 'Exception SMTPRecipientsRefused'
+			print('Exception SMTPRecipientsRefused')
 			pass		
 		except smtplib.SMTPDataError:
-			print 'Exception SMTPDataError'
+			print('Exception SMTPDataError')
 			pass		
 		except smtplib.SMTPConnectError:
-			print 'Exception SMTPConnectError'
+			print('Exception SMTPConnectError')
 			pass		
 		except smtplib.SMTPHeloError:
-			print 'Exception SMTPHeloError'
+			print('Exception SMTPHeloError')
 			pass		
 		except smtplib.SMTPAuthenticationError:
-			print 'Exception SMTPAuthenticationError'
+			print('Exception SMTPAuthenticationError')
 			pass
 		except:
-			print 'Undefined exception'
-			print traceback.format_exc()
+			print('Undefined exception')
+			print(traceback.format_exc())
 
 		return
 		
-server = CustomSMTPServer((HOST, PORT), None)
+server = CustomSMTPServer((FILTER_HOST, FILTER_PORT), None)
 
 asyncore.loop()
